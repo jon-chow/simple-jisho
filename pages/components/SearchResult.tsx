a17053f93406d692c6764c2928effbec395e74f7
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-11-25 22:24:34 -0800

    created SearchResult component

---
+++ pages/components/SearchResult.tsx
@@ -0,0 +1,36 @@
+const Panel = (props: any) => {
+  return (
+    <div className="panel">
+      <h1>{props.title}</h1>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+const Result = (props: any) => {
+  return (
+    <div className="result">
+      <h2>{props.title}</h2>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+
+/* -------------------------------------------------------------------------- */
+/*                             EXPORTED COMPONENT                             */
+/* -------------------------------------------------------------------------- */
+/**
+ * Exports the SearchResult component.
+ * 
+ * The SearchResult component contains data on a search.
+ */
+export default function SearchResult(props: any) {
+  return (
+    <Panel title="Search Result">
+      <Result title="Result 1" />
+      <Result title="Result 2" />
+      <Result title="Result 3" />
+    </Panel>
+  );
+};
