5221bc772da042f69a328f8d788204661fb2b445
Jon Chow <70170720+jon-chow@users.noreply.github.com>
2022-12-09 00:59:18 -0800

    added jisho link to panels

--- pages/components/SearchResult.tsx
+++ pages/components/SearchResult.tsx
@@ -14,113 +14,125 @@ export default function SearchResult(props: any) {
   console.log(props.resultData);
   
   return (
-    <div className={styles.resultPanel}>
-      {/* JLPT / Rarity Tags */}
-      <div className={styles.tags}>
-        { (data.is_common) ? 
-          <span className={styles.common} style={{background: 'rgba(50, 255, 50, 0.4)'}}>Common</span> :
-          <span className={styles.common} style={{background: 'rgba(50, 50, 255, 0.4)'}}>Uncommon</span>
-        }
-        { data.jlpt.map((level: string, index: number) => {
-          const levelNum = level.replace('jlpt-n', '');
-          let colour = 'rgba(255, 50, 50, 0.4)';
-
-          switch(levelNum) {
-            case '1':
-              colour = 'rgba(255, 50, 50, 0.4)';
-              break;
-            case '2':
-              colour = 'rgba(255, 100, 50, 0.4)';
-              break;
-            case '3':
-              colour = 'rgba(255, 255, 50, 0.4)';
-              break;
-            case '4':
-              colour = 'rgba(50, 255, 50, 0.4)';
-              break;
-            default:
-              colour = 'rgba(50, 50, 255, 0.4)';
+    <article className={styles.resultPanel}>
+      {/* Jisho Search */}
+      <section className={styles.jishoSearch}>
+        {/* JLPT / Rarity Tags */}
+        <div className={styles.tags}>
+          { (data.is_common) ? 
+            <span className={styles.common} style={{background: 'rgba(50, 255, 50, 0.4)'}}>Common</span> :
+            <span className={styles.common} style={{background: 'rgba(50, 50, 255, 0.4)'}}>Uncommon</span>
           }
+          { data.jlpt.map((level: string, index: number) => {
+            const levelNum = level.replace('jlpt-n', '');
+            let colour = 'rgba(255, 50, 50, 0.4)';
 
-          return (
-            <span key={index} className={styles.jlpt} style={{background: colour}}>
-              JLPT N{levelNum}
-            </span>
-          )
-        })}
-      </div>
-
-      {/* Main Form */}
-      <div className={styles.entry}>
-        { (mainForm.word) ?
-          <>
-            {mainForm.reading && <div className={styles.reading}>{mainForm.reading}</div>}
-            <div className={styles.word}>{mainForm.word}</div>
-          </> :
-          <div className={styles.word}>{mainForm.reading}</div>
-        }
-      </div>
-
-      {/* Senses */}
-      <div className={styles.senses}>
-        {data.senses.map((sense: any, index: number) => {
-          return (
-            <div key={index} className={styles.sense}>
-              {sense.parts_of_speech.map((pos: string, index: number) => {
-                return (
-                  <span key={index} className={styles.pos}>
-                    {pos}
-                  </span>
-                )
-              })}
-              
-              {sense.english_definitions.map((definition: string, index: number) => {
-                return (
-                  <span key={index} className={styles.definition}>
-                    {definition}
-                  </span>
-                )
-              })}
-            </div>
-          )
-        })}
-      </div>
-      
-      {/* Other Forms */}
-      { (data.japanese.length > 1) && (
-        <div className={styles.otherForms}>
-          <br />
-          Other Forms:
-
-          <div className={styles.forms}>
-            { data.japanese.map((japanese: any, index: number) => {
-                if (index === 0) return;
-
-                return (
-                  <span key={index} className={styles.form}>
-                      { (japanese.word) ?
-                        <>
-                          <span className={styles.formWord}>
-                            {japanese.word}
-                            { (japanese.reading) && 
-                              <span>
-                                {`【${japanese.reading}】`}
-                              </span>
-                            }
-                          </span>
-                        </> :
-                        <span className={styles.formWord}>
-                          {`${japanese.reading}`}
-                        </span>
-                      }
-                      <span>{(index != data.japanese.length - 1) ? ' 、' : ''}</span>
-                  </span>
-                )
-              })
+            switch(levelNum) {
+              case '1':
+                colour = 'rgba(255, 50, 50, 0.4)';
+                break;
+              case '2':
+                colour = 'rgba(255, 100, 50, 0.4)';
+                break;
+              case '3':
+                colour = 'rgba(255, 255, 50, 0.4)';
+                break;
+              case '4':
+                colour = 'rgba(50, 255, 50, 0.4)';
+                break;
+              default:
+                colour = 'rgba(50, 50, 255, 0.4)';
             }
-          </div>
+
+            return (
+              <span key={index} className={styles.jlpt} style={{background: colour}}>
+                JLPT N{levelNum}
+              </span>
+            )
+          })}
         </div>
-      )}
-    </div>
+
+        {/* Main Form */}
+        <div className={styles.entry}>
+          { (mainForm.word) ?
+            <>
+              {mainForm.reading && <div className={styles.reading}>{mainForm.reading}</div>}
+              <div className={styles.word}>{mainForm.word}</div>
+            </> :
+            <div className={styles.word}>{mainForm.reading}</div>
+          }
+        </div>
+
+        {/* Senses */}
+        <div className={styles.senses}>
+          {data.senses.map((sense: any, index: number) => {
+            return (
+              <div key={index} className={styles.sense}>
+                {sense.parts_of_speech.map((pos: string, index: number) => {
+                  return (
+                    <span key={index} className={styles.pos}>
+                      {pos}
+                    </span>
+                  )
+                })}
+                
+                {sense.english_definitions.map((definition: string, index: number) => {
+                  return (
+                    <span key={index} className={styles.definition}>
+                      {definition}
+                    </span>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+        
+        {/* Other Forms */}
+        { (data.japanese.length > 1) && (
+          <div className={styles.otherForms}>
+            <br />
+            Other Forms:
+
+            <div className={styles.forms}>
+              { data.japanese.map((japanese: any, index: number) => {
+                  if (index === 0) return;
+
+                  return (
+                    <span key={index} className={styles.form}>
+                        { (japanese.word) ?
+                          <>
+                            <span className={styles.formWord}>
+                              {japanese.word}
+                              { (japanese.reading) && 
+                                <span>
+                                  {`【${japanese.reading}】`}
+                                </span>
+                              }
+                            </span>
+                          </> :
+                          <span className={styles.formWord}>
+                            {`${japanese.reading}`}
+                          </span>
+                        }
+                        <span>{(index != data.japanese.length - 1) ? ' 、' : ''}</span>
+                    </span>
+                  )
+                })
+              }
+            </div>
+          </div>
+        )}
+      </section>
+
+      
+
+      {/* Jisho Link */}
+      <section className={styles.jishoLink}>
+        <a href={`https://jisho.org/search/${data.slug}`} target="_blank" rel="noreferrer">
+          View on Jisho.org ≫
+        </a>
+      </section>
+    </article>
   );
 };
